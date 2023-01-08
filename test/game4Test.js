39959faedcdc0c8f6f852738213b6b660bea2933
jarryingnut <rabhishekranjan@gmail.com>
2023-01-08 15:42:47 +0530

    do contract1 to contract4

---
+++ test/game4Test.js
@@ -0,0 +1,31 @@
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
+const { assert } = require("chai");
+
+describe("Game4", function () {
+  async function deployContractAndSetVariables() {
+    const Game = await ethers.getContractFactory("Game4");
+    const game = await Game.deploy();
+
+    const signer1 = ethers.provider.getSigner(0);
+    const signer2 = ethers.provider.getSigner(1);
+    const signer3 = ethers.provider.getSigner(2);
+    const address1 = await signer1.getAddress();
+    const address2 = await signer2.getAddress();
+    const address3 = await signer3.getAddress();
+
+    return { game, signer1, address1, signer2, address2 };
+  }
+  it("should be a winner", async function () {
+    const { game, signer1, address1, signer2, address2 } = await loadFixture(
+      deployContractAndSetVariables
+    );
+
+    // nested mappings are rough :}
+    await game.connect(signer1).write(address2);
+    //await game.connect(signer2).write(address1);
+    await game.connect(signer2).win(address1);
+
+    // leave this assertion as-is
+    assert(await game.isWon(), "You did not win the game");
+  });
+});
